469763b9b03769d1f5c17c72648d336871879b88
dudxo <father8475@naver.com>
2023-06-23 03:25:01 +0900

    [Update] KMP 주석 추가

--- src/Algorithm/KMP.java
+++ src/Algorithm/KMP.java
@@ -13,18 +13,23 @@ public class KMP {
     }
 
     static void kmp(String parent, String pattern) {
-        int[] makeTable = table(pattern);
-        char[] parents = parent.toCharArray();
-        char[] patterns = pattern.toCharArray();
+        int[] makeTable = table(pattern);       // 테이블(실패 함수)
+        char[] parents = parent.toCharArray();      // 전체 문자열
+        char[] patterns = pattern.toCharArray();    // 부분 문자열
 
         int k = 0;
         for (int i = 1; i < parents.length; i++) {
+            // k가 0보다 크고 전체 문자열의 문자와 패턴 문자열의 문자가 다르면
+            // k를 직전 위치로 이동
             while (k > 0 && parents[i] != patterns[k]) {
                 k = makeTable[k - 1];
             }
+            // 둘의 문자가 같을 때
+            // k가 패턴 문자열의 길이와 같으면 일치  == 패턴 문자열 처음부터 끝까지 같으면 문자열 일치
+            // 다르면 k 값을 증가시켜 접두사까지 확인
             if (parents[i] == patterns[k]) {
                 if(k == patterns.length - 1) {
-                    System.out.printf("%d 번째에서 찾았습니다.", i - patterns.length + 2);
+                    System.out.printf("%d 번째에서 찾았습니다.\n", i - patterns.length + 2);
                     k = makeTable[k];
                 }
                 else {
@@ -34,7 +39,7 @@ public class KMP {
         }
     }
 
-    static int[] table(String pattern) {
+    static int[] table(String pattern) {        // 접두사, 접미사 일치 테이블 메서드
         char[] patterns = pattern.toCharArray();
         int[] table = new int[patterns.length];
         int k = 0;
