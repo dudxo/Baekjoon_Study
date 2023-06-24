c03fddb2c23e8fe94efea82992d0208873d0f3aa
dudxo <father8475@naver.com>
2023-06-24 12:46:40 +0900

    [Update] Baekjoon1919 코드 함수화

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 import java.io.*;
 
 public class Main {
+    public static int[] getCount(String s) {
+        int[] count = new int[26];
 
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bw = new BufferedReader(new InputStreamReader(System.in));
+
+        String a = bw.readLine();
+        String b = bw.readLine();
+
+        int[] a1 = getCount(a);
+        int[] b1 = getCount(b);
+
+        int ans = 0;
+        for (int k = 0; k < 26; k++) {
+            ans += Math.abs(a1[k] - b1[k]);
+        }
+
+        System.out.println(ans);
+    }
 }
--- src/baekjoon/Baekjoon1919.java
+++ src/baekjoon/Baekjoon1919.java
@@ -11,32 +11,53 @@ import java.io.InputStreamReader;
  * Sol
  */
 public class Baekjoon1919 {
+
+    public static int[] getCount(String s) {
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+        }
+
+        return count;
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader bw = new BufferedReader(new InputStreamReader(System.in));
 
         String a = bw.readLine();
         String b = bw.readLine();
 
-        int[] a1 = new int[26];
-        int[] b1 = new int[26];
+        int[] a1 = getCount(a);
+        int[] b1 = getCount(b);
 
+        /**
+         * getCount로 함수화
+         */
+/*
         for (int i = 0; i < a.length(); i++) {
             a1[a.charAt(i) - 'a']++;        // 알파벳 순으로 값 저장
         }
         for (int i = 0; i < b.length(); i++) {
             b1[b.charAt(i) - 'a']++;        // 알파벳 순으로 값 저장
         }
+*/
 
         int ans = 0;
         for (int k = 0; k < 26; k++) {
             // a1[k]와 b1[k]의 값 즉 알파벳 개수가 다르면 다른 만큼 제거
-            if (a1[k] != b1[k]) {
+            ans += Math.abs(a1[k] - b1[k]);
+
+            /**
+             * Math 클래스를 이용해 절대값 로직 단축
+             */
+/*            if (a1[k] != b1[k]) {
                 if (a1[k] - b1[k] > 0) {
                     ans += a1[k] - b1[k];
                 } else {
                     ans += ((a1[k] - b1[k]) * -1);
                 }
-            }
+            }*/
         }
 
         System.out.println(ans);
