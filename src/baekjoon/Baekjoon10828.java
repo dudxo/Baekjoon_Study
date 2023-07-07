e51f9b42df65a7bb731ce03d814c68f6b7f04e33
dudxo <father8475@naver.com>
2023-07-07 23:40:19 +0900

    [23-07-07] 백준10828 Solved

--- src/baekjoon/Baekjoon10828.java
+++ src/baekjoon/Baekjoon10828.java
@@ -1,99 +1,119 @@
 package baekjoon;
+
 import java.io.*;
 
-class Node<T> {
-    Node<T> next = null;
-    T data = null;
+class Node<T> {     // T타입 노드
+    Node<T> next = null;        // 다음 노드를 지정할 next null 초기화
+    T data = null;      // 노드의 data null 초기화
 }
 
 class Stack<T> {
-    Node<T> head = null;
+    Node<T> head = null;        // head(항상 마지막 Node를 가르킴) null 초기화
 
-    public boolean isempty() {
+    int top = -1;       // top -1 초기화
+    public boolean isempty() {      // Stack 공백 체크  * 시간복잡도 : O(1)
         return head == null;
     }
 
-    public void push(T data) {
-        Node<T> newNode = new Node<>();
-        newNode.data = data;
+    public void push(T data) {      // data push 연산  * 시간복잡도 : O(1)
+        Node<T> newNode = new Node<>();     // newNode 생성
+        newNode.data = data;        // newNode의 push할 data 대입
 
-        if (!isempty()) {
-            newNode.next = this.head;
+        if (!isempty()) {       // 공백이 아닌 즉 이미 다른 Node가 있다면
+            newNode.next = this.head;       // head가 가르키는 주소 즉 맨 마지막 Node를 newNode가 가르키도록 대입
         }
-        this.head = newNode;
+        this.head = newNode;        // head는 newNode를 참조
+        top++;      // top 증가
     }
 
-    public T pop() {
-        T tmp = this.head.data;
-        Node<T> popedNode = this.head;
-        this.head = this.head.next;
-
-        popedNode.next = null;
-        popedNode.data = null;
-        return tmp;
-    }
-
-    public int size() {
-        int size = 0;
-        while (!isempty()) {
-            pop();
-            size++;
-        }
-        return size;
-    }
-
-    public int top() {
-        if (isempty()) {
+    public int pop() {      // data pop 연산  * 시간복잡도 : O(1)
+        if (isempty()) {        // 공백이라면 return -1
             return -1;
         }
-        return (int) this.head.data;
+        T tmp = this.head.data;     // 꺼내올 top data를 임시 변수 T의 저장
+        Node<T> popedNode = this.head;      // 삭제될 노드(head가 가르키는 곳)를 popeNode로 지정
+        this.head = this.head.next;     // head는 삭제될 노드의 다음 노드를 가르킴
+
+        popedNode.next = null;      // 삭제될 노드의 next(다음 노드 값) null 대입
+        popedNode.data = null;      // 삭제될 노드의 data(노드 data) null 대입해 삭제
+        top--;      // top 감소
+        return (int) tmp;       // 삭제 될 data(pop연산 전 top data) return
+
+    }
+
+    public int size() {     // size == Stack 크기  * 시간복잡도 : O(1)
+        return top+1;       // Stack의 크기 return
+    }
+
+    public int top() {      // top data return  * remove X  * 시간복잡도 : O(1)
+        if (isempty()) {        // 공백이라면 return -1
+            return -1;
+        }
+        return (int) this.head.data;        // top data return
     }
 }
 
+/**
+ * 총 시간 복잡도
+ * while : O(N)
+ * stack : O(1)
+ * = O(N)..?
+ */
 public class Baekjoon10828 {
-    public static void main(String[] args) throws IOException{
+
+    static Stack<Integer> stack = new Stack<>();        // sol()메소드에서도 사용하기 때문에 전역 변수 지정
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());        // 명령의 수
 
-        String cmd = "";
-        int data = 0;
+        String cmd = "";        // 명령어가 담길 지역변수 cmd
 
-        Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < N; i++) {
-            cmd = br.readLine();
-            if (cmd.contains("push")) {
-                data = Integer.parseInt(cmd.substring(5));
-                cmd = cmd.substring(0, 4);
-            }
-            switch (cmd) {
-                case "push":
-                    stack.push(data);
-                    break;
-                case "pop":
-                    if (!stack.isempty()) {
-                        bw.write(Integer.toString(stack.pop()));
-                    } else {
-                        bw.write("-1");
-                    }
-
-                    break;
-                case "size":
-                    bw.write(Integer.toString(stack.size()));
-                    break;
-                case "empty":
-                    if (stack.isempty()) {
-                        bw.write("1");
-                    } else {
-                        bw.write("0");
-                    }
-                    break;
-                case "top":
-                    bw.write(Integer.toString(stack.top()));
-            }
-            bw.flush();
+        while (N-- > 0) {       // 명령의 수만큼 반복
+            cmd = br.readLine();        // 명령어 입력
+            sol(cmd);       // 명령 수행
         }
     }
+
+    /**
+     *  명령어를 해석해 Stack 메소드 실행
+     */
+    public static void sol(String cmd) throws IOException {
+
+        int data = 0;      // push 될 지역변수 data
+
+        if (cmd.contains("push")) {     // push 명령어 분리 로직
+            data = Integer.parseInt(cmd.substring(5));      // push 뒤 data 값 추출
+            cmd = cmd.substring(0, 4);      // 명령어에 push만 남김
+        }
+        switch (cmd) {      // 해당 명령어 실행 로직
+            case "push":
+                stack.push(data);
+                break;
+            case "pop":
+                bw.write(Integer.toString(stack.pop()));
+                bw.newLine();       // 한 줄에 하나씩 출력하기 위해 newLine() 사용
+                break;
+            case "size":
+                bw.write(Integer.toString(stack.size()));
+                bw.newLine();
+                break;
+            case "empty":
+                if (stack.isempty()) {
+                    bw.write("1");
+                } else {
+                    bw.write("0");
+                }
+                bw.newLine();
+                break;
+            case "top":
+                bw.write(Integer.toString(stack.top()));
+                bw.newLine();
+                break;
+        }
+        bw.flush();
     }
 }
--- src/baekjoon/Baekjoon5397.java
+++ src/baekjoon/Baekjoon5397.java
@@ -14,56 +14,61 @@ public class Baekjoon5397 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int T = Integer.parseInt(br.readLine());
-        LinkedList<String> st_pw = new LinkedList<String>();
+
+        int T = Integer.parseInt(br.readLine());    // 테스트 케이스 개수
+
+        LinkedList<String> st_pw = new LinkedList<String>();        // 훔친 비밀번호를 담을 지역변수 st_pw
 
 
-        while (T != 0) {
-            int location = 1;
-            String pw = br.readLine();
-            String[] pw2 = pw.split("");
+        while (T != 0) {        // 테스트 케이스만큼 반복
+            int location = 1;       // 커서 위치 1로 지정
+            String pw = br.readLine();      // passWard 입력
+            String[] pw2 = pw.split("");        // 입력된 passWard 쪼개기
             st_pw.clear();
-            for(int i = 0; i < pw2.length; i++){
-                if (pw2[i].equals("<")) {
-                    if (location > 1) {
-                        location--;
+            for(int i = 0; i < pw2.length; i++){        // passWard 길이만큼 반복
+                if (pw2[i].equals("<")) {       // "<" 일 때
+                    if (location > 1) {     // 커서가 왼쪽으로 이동할 수 있는 위치라면
+                        location--;     // 왼쪽으로 한 칸 이동
                     }
                 }
-                else if (pw2[i].equals(">")) {
-                    if (location < st_pw.size() + 1) {
-                        location++;
+                else if (pw2[i].equals(">")) {      // ">" 일 때
+                    if (location < st_pw.size() + 1) {      // 현재 복사된 st_pw 길이보다 작으면
+                        // location은 1부터 시작하기 때문에 size()+1
+                        location++;     // 오른쪽으로 한 칸 이동
                     }
                 }
                 else {
-                    if (pw2[i].equals("-")) {
-                        if (!st_pw.isEmpty() && location > 1) {
+                    if (pw2[i].equals("-")) {       // "-" 일 때
+                        if (!st_pw.isEmpty() && location > 1) {     // st_pw가 공백이 아니고 커서 위치가 맨 처음이 아닐 때
                             if (location - 1 == st_pw.size()) {
-                                st_pw.removeLast();
-                                location--;
+                                /* 또한 현재 커서 위치-1이 st_pw 크기와 같다면
+                                한 글자만 st_pw에 있는것과 같다 */
+                                st_pw.removeLast();     // 제일 마지막 삭제하고
+                                location--;     // 커서 위치 맨 처음으로 초기화
                             } else {
-                                st_pw.remove(location-2);
-                                location--;
+                                st_pw.remove(location-2);       // 커서 전 위치에 있는 문자를 지워야되니 커서위치-2값을 지운다
+                                location--;     // 왼쪽으로 한 칸 이동
                             }
                         }
                     }
-                    else {
-                        if (location > st_pw.size() + 1) {
-                            st_pw.add(location - 2, pw2[i]);
+                    else {      // 이외 문자
+                        if (location > st_pw.size() + 1) {      // 커서위치가 맨 끝이라면
+                            st_pw.add(location - 2, pw2[i]);        // 커서 위치-2 자리에 넣고
                         } else {
-                            st_pw.add(location - 1, pw2[i]);
-                            location++;
+                            st_pw.add(location - 1, pw2[i]);        // 맨 끝이 아닌 처음 또는 중간이라면 커서 위치-1 자리에 넣는다
+                            location++;     // 커서 오른쪽으로 이동
                         }
                     }
                 }
             }
 
-            ArrayList<String> sol = new ArrayList<>(st_pw);
-            for (String pr : sol) {
-                bw.write(pr);
+            ArrayList<String> sol = new ArrayList<>(st_pw);    // get의 시간복잡도가 O(1)인 ArrayList로 변환
+            for (int i = 0; i<sol.size(); i++) {
+                bw.write(sol.get(i));       // sol.get
             }
             bw.flush();
             bw.newLine();
-            T--;
+            T--;        // 다음 테스트로 이동을 위한 후위감소식
         }
         bw.close();
 
