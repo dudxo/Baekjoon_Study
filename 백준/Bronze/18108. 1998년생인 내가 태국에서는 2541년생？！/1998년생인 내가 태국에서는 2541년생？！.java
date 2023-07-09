a17f5131d53fe30c2e8cc9d6d36570cd15b43f4f
권영태 <56019823+dudxo@users.noreply.github.com>
2023-07-10 02:26:32 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 208 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        int year = 0;
+        year = sc.nextInt();
+        System.out.println(year - 543);
+    }
+
+}
