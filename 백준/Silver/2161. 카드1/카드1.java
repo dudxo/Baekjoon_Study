5b3594cd00538acf4fcbe7559dd90a6c67e9d549
권영태 <56019823+dudxo@users.noreply.github.com>
2023-07-09 11:33:44 +0900

    [Silver V] Title: 카드1, Time: 132 ms, Memory: 14364 KB -BaekjoonHub

---
+++ 백준/Silver/2161. 카드1/카드1.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.Deque;
+import java.util.LinkedList;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Deque<Integer> queue = new LinkedList<>();
+
+        int N = 0;
+        N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            queue.add(i);
+        }
+
+        while(queue.size() != 1) {
+            bw.write(Integer.toString(queue.pop()));
+            bw.write(" ");
+            queue.addLast(queue.pop());
+        }
+
+        bw.write(Integer.toString(queue.peek()));
+        bw.flush();
+        bw.close();
+
+
+    }
+
+}
