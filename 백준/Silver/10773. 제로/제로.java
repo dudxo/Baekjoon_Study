8cf8d000a8d9d49491d84fde3382c69a5ae20abf
권영태 <56019823+dudxo@users.noreply.github.com>
2024-01-26 16:59:29 +0900

    [Silver IV] Title: 제로, Time: 252 ms, Memory: 24284 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.Stack;
+
+/**
+ * 시간제한 1초, 입력값의 범위 1~100,000
+ * O(NlogN)이하 알고리즘 설계
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = 0, sol = 0;
+        T = Integer.parseInt(br.readLine());
+        Stack<Integer> stack =new Stack<>();
+
+        while (T-- > 0) {
+            int money = 0;
+            money = Integer.parseInt(br.readLine());
+            if (money != 0) {
+                stack.push(money);
+            } else {
+                stack.pop();
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            sol += stack.pop();
+        }
+        bw.write(Integer.toString(sol));
+        bw.flush();
+        bw.close();
+    }
+}
