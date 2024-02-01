f6a0580b287dc5d4198746e6f8342faa19f4058f
권영태 <56019823+dudxo@users.noreply.github.com>
2024-02-01 17:16:51 +0900

    [Silver I] Title: 외계인의 기타 연주, Time: 1888 ms, Memory: 394700 KB -BaekjoonHub

--- 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.java
+++ 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.java
@@ -16,22 +16,16 @@ public class Main {
         for (int i = 1; i <= N; i++) {
             int n = sc.nextInt();
             int p = sc.nextInt();
-            if (stack[n].size() == 0) {
-                stack[n].push(p);
-                count++;
-                continue;
-            }
 
             while (!stack[n].isEmpty() && stack[n].peek() > p) {
                 stack[n].pop();
                 count++;
             }
 
-            if (stack[n].size() !=0 && stack[n].peek() == p) {
-                continue;
+            if (stack[n].size() == 0 || stack[n].peek() < p) {
+                stack[n].push(p);
+                count++;
             }
-            stack[n].push(p);
-            count++;
         }
         System.out.println(count);
     }
