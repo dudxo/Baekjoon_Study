545193c8e276c3aebae92e37e405875be41a97ea
권영태 <56019823+dudxo@users.noreply.github.com>
2024-03-19 10:31:45 +0900

    [Gold V] Title: AC, Time: 848 ms, Memory: 102528 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -1,49 +1,76 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
 
-        for (int tc = 1; tc <= T; tc++) {
-            String p = sc.next();
-            int n = sc.nextInt();
-
-            String arrStr = sc.next();
+    static char[] arr;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
             Deque<Integer> deque = new LinkedList<>();
-            for (String s : arrStr.substring(1, arrStr.length() - 1).split(","))
-                if (!s.equals(""))
-                    deque.add(Integer.valueOf(s));
+            String p = br.readLine();   // 명령어
+            String n = br.readLine();
 
-            System.out.println(ac(deque, p));
+            String str = br.readLine();
+            arr = new char[str.length()];
+
+            for (String s : str.substring(1, str.length() - 1).split(",")) {
+                if (!s.equals("")) {
+                    deque.add(Integer.valueOf(s));
+                }
+            }
+            System.out.println(command(p, deque));
+
         }
+
     }
 
-    static String ac(Deque<Integer> deque, String commands) {
-        boolean reverse = false;
+    static String command(String p, Deque<Integer> deque) {
+        //check가 true라면 앞에서
+        //check가 false라면 뒤에서
+        boolean check = true;
 
-        for (char command : commands.toCharArray()) {
-            if (command == 'R')
-                reverse = !reverse;
-            else {
-                if (deque.size() == 0)
-                    return "error";
+        for(int i = 0; i < p.length(); i++){
+            char c = p.charAt(i);
+            switch (c) {
+                case 'R':
+                    check = !check;
+                    break;
+                case 'D':
+                    if(deque.size() == 0){
+                        return "error";
+                    }
 
-                if (reverse)
-                    deque.removeLast();
-                else
-                    deque.removeFirst();
+                    if (!check) {
+                        deque.removeLast();
+                    } else {
+                        deque.removeFirst();
+                    }
+                    break;
             }
         }
-
         StringBuilder sb = new StringBuilder("[");
-        while (!deque.isEmpty()) {
-            sb.append(reverse ? deque.removeLast() : deque.removeFirst());
-            if (deque.size() != 0)
-                sb.append(',');
+        if (check) {
+            while (!deque.isEmpty()){
+                sb.append(deque.removeFirst());
+                if(!deque.isEmpty()) {
+                    sb.append(",");
+                }
+            }
+            sb.append("]");
+        } else {
+            while (!deque.isEmpty()){
+                sb.append(deque.removeLast());
+                if(!deque.isEmpty()) {
+                    sb.append(",");
+                }
+            }
+            sb.append("]");
         }
-        sb.append(']');
-
         return sb.toString();
     }
 }
