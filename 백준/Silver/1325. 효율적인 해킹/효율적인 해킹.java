c5ae24ac8fc9e5d843161e92ecf7eaba7d5c7ea7
권영태 <56019823+dudxo@users.noreply.github.com>
2024-09-26 17:07:11 +0900

    [Silver I] Title: 효율적인 해킹, Time: 10676 ms, Memory: 147700 KB -BaekjoonHub

---
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static ArrayList<Integer>[] A;
+    private static int[] answer;
+    private static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        A = new ArrayList[N+1];
+        answer = new int[N+1];
+
+        for(int i = 1; i <= N; i++) {
+            A[i] = new ArrayList<Integer>();
+        }
+
+        for(int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int S = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
+            A[S].add(E);
+        }
+
+        for(int i = 1; i <= N; i++) {
+            visited = new boolean[N+1];
+            DFS(i);
+        }
+
+        int max = Arrays.stream(answer).max().getAsInt();
+
+        for(int i = 1; i <= N; i++) {
+            if(max == answer[i]) {
+                bw.write(i + " ");
+            }
+        }
+
+        bw.flush();
+        bw.close();
+    }
+
+    private static void DFS(int S) {
+        if(visited[S]) {
+            return;
+        }
+        visited[S] = true;
+        answer[S]++;
+        for(int i : A[S]) {
+            if(!visited[i]) {
+                DFS(i);
+            }
+        }
+    }
+}
