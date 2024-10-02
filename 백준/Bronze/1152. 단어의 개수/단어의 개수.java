74aff2144916a65aea11dfc071c2f9983a78c55d
권영태 <56019823+dudxo@users.noreply.github.com>
2024-10-02 22:27:24 +0900

    [Bronze II] Title: 단어의 개수, Time: 504 ms, Memory: 61756 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String[] str = sc.nextLine().split(" ");
+        System.out.println(Arrays.stream(str).filter(e -> e != "").count());
+
+    }
+
+
+
+}
+
+
