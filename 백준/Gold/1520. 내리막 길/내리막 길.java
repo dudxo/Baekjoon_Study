95020f8d0cbd1f22b4f5322af4892c0889371c85
권영태 <56019823+dudxo@users.noreply.github.com>
2024-11-28 20:38:51 +0900

    [Gold III] Title: 내리막 길, Time: 1044 ms, Memory: 214608 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -2,19 +2,21 @@ import java.util.Scanner;
 
 public class Main {
 
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {1, 0, -1, 0};
-    static int N, M;
-    static int[][] map;
-    static int[][] dp;
-    public static void main(String[] args){
+    static int N, M, count;
+    static boolean[][] visited;
+    static int[][] dp, map;
+    static int[] dx = {0,1,0,-1};
+    static int[] dy = {1,0,-1,0};
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         M = sc.nextInt();
         N = sc.nextInt();
-        sc.nextLine();
 
-        map = new int[M][N];
+        count = 0;
+
         dp = new int[M][N];
+        map = new int[M][N];
+        visited = new boolean[M][N];
 
         for(int i = 0; i < M; i++) {
             for(int j = 0; j < N; j++) {
@@ -22,35 +24,35 @@ public class Main {
                 dp[i][j] = -1;
             }
         }
-        System.out.print(DFS(0, 0));
+
+
+        System.out.println(DFS(0, 0));
     }
 
-
-
     private static int DFS(int i, int j) {
-       if(i == M-1 && j ==N-1) {
-           return 1;
-       }
+        if(i == M-1 && j == N-1) {
+            return 1;
+        }
 
-       if(dp[i][j] != -1) {
-           return dp[i][j];
-       }
+        if(dp[i][j] != -1) {
+            return dp[i][j];
+        }
 
-       dp[i][j] = 0;
+        dp[i][j] = 0;
 
-       for(int k = 0; k < 4; k++) {
-           int x = i + dx[k];
-           int y = j + dy[k];
+        for(int k = 0; k < 4; k++) {
+            int x = i + dx[k];
+            int y = j + dy[k];
 
-           if(x < 0 || y < 0 || x >= M || y >= N) {
-               continue;
-           }
+            if(x < 0 || y < 0 || x >= M || y >= N) {
+                continue;
+            }
 
-           int next = map[x][y];
-           if(map[i][j] > next) {
+            if(map[i][j] > map[x][y]) {
                dp[i][j] += DFS(x, y);
-           }
-       }
+            }
+        }
+
         return dp[i][j];
     }
 }
