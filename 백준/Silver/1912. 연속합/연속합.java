03ddce7cdd7ae084898b78d43d6d89234e203543
권영태 <56019823+dudxo@users.noreply.github.com>
2024-12-06 16:20:04 +0900

    [Silver II] Title: 연속합, Time: 708 ms, Memory: 127748 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+
+public class Main {
+
+    static int n;
+    static int[] sequence;
+    static Integer[] dp;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        sequence = new int[n];
+        dp = new Integer[n];
+        for(int i = 0; i < n; i++) {
+            sequence[i] = sc.nextInt();
+        }
+        sum(0);
+
+        int max = dp[0];
+        for(int i : dp) {
+            if(i > max) {
+                max = i;
+            }
+        }
+        System.out.println(max);
+    }
+
+    private static int sum(int i) {
+        if(i >= n) {
+            return 0;
+        }
+
+        if(dp[i] == null) {
+            dp[i] = 0;
+            if(i < n-1) {
+                dp[i] = Math.max(sum(i+1) + sequence[i], sequence[i]);
+            }
+            if(i == n-1) {
+                dp[i] = sequence[i];
+            }
+        }
+
+        return dp[i];
+    }
+}
