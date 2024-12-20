332d438b109c5c65f1f4c831aa2ac65847a1cc31
권영태 <56019823+dudxo@users.noreply.github.com>
2024-12-20 17:43:49 +0900

    [Silver III] Title: N과 M (6), Time: 108 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+
+    static int N, M;
+    static int[] arr, nums;
+    static boolean[] isUsed;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer str = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(str.nextToken());
+        M = Integer.parseInt(str.nextToken());
+
+        arr = new int[M];
+        nums = new int[N];
+        isUsed = new boolean[N];
+
+        str = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(str.nextToken());
+        }
+
+        Arrays.sort(nums);
+
+        br.close();
+
+        solve(0);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    private static void solve(int k) {
+        if(k == M) {
+            for(int index : arr) {
+                sb.append(index).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = 0; i < N; i++) {
+            if(isUsed[i]) {
+                continue;
+            }
+
+            if(k > 0 && arr[k-1] > nums[i]) {
+                continue;
+            }
+
+            arr[k] = nums[i];
+            isUsed[i] = true;
+            solve(k+1);
+            isUsed[i] = false;
+        }
+    }
+
+}
