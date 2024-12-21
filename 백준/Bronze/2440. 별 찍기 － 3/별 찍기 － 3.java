06b7c0ff8db7b113388203300c7b93887f8999c0
권영태 <56019823+dudxo@users.noreply.github.com>
2024-12-22 00:36:40 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 100 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+
+    static int N;
+    static StringBuilder sb;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        sb = new StringBuilder();
+        StringTokenizer str = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(str.nextToken());
+
+        star(N);
+        bw.write(sb.toString());
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+
+    private static void star(int n) {
+        for(int i = n; i > 0; i--) {
+            for(int j = i; j > 0; j--) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+    }
+}
