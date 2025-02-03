ea177f275387e928850d03e9e420d36ecfe0f5d5
권영태 <56019823+dudxo@users.noreply.github.com>
2025-02-03 16:13:55 +0900

    [Gold V] Title: 택배 배송, Time: 412 ms, Memory: 40408 KB -BaekjoonHub

---
+++ 백준/Gold/5972. 택배 배송/택배 배송.java
@@ -0,0 +1,83 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N, M;
+    static int INF = Integer.MAX_VALUE;
+    static int[] dist;
+    static ArrayList<Node>[] graph;
+    public static class Node implements Comparable<Node> {
+        int index;
+        int cost;
+
+        Node(int index, int cost) {
+            this.index = index;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return Integer.compare(this.cost, o.cost);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer str = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(str.nextToken());
+        M = Integer.parseInt(str.nextToken());
+
+        dist = new int[N+1];
+        graph = new ArrayList[N+1];
+
+        for(int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for(int i = 0; i < M; i++) {
+            str = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(str.nextToken());
+            int v = Integer.parseInt(str.nextToken());
+            int w = Integer.parseInt(str.nextToken());
+
+            graph[u].add(new Node(v, w));
+            graph[v].add(new Node(u, w));
+        }
+
+        dijkstra(1);
+
+        sb.append(dist[N]);
+
+        bw.write(sb.toString());
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+
+    private static void dijkstra(int start) {
+        Arrays.fill(dist, INF);
+        PriorityQueue<Node> q = new PriorityQueue<>();
+        q.add(new Node(start, 0));
+        dist[start] = 0;
+
+        while(!q.isEmpty()) {
+            Node now = q.poll();
+
+            if(now.cost > dist[now.index]) {
+                continue;
+            }
+
+            for(Node next : graph[now.index]) {
+                if(dist[next.index] > dist[now.index] + next.cost) {
+                    dist[next.index] = dist[now.index] + next.cost;
+                    q.add(new Node(next.index, dist[next.index]));
+                }
+            }
+        }
+    }
+
+}
