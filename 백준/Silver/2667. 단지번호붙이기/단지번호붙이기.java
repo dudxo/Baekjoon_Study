a7272fa7ad1a12c4168b2ca6a398d6ebfd2e8f98
권영태 <56019823+dudxo@users.noreply.github.com>
2025-02-07 15:26:54 +0900

    [Silver I] Title: 단지번호붙이기, Time: 100 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,49 +1,67 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-import java.util.Scanner;
+
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    static int[] dx = {0,1,0,-1};
-    static int[] dy = {1,0,-1,0};
-    static int N, count;
-    static char[][] apt;
+    static int N;
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+    static int[][] map;
     static boolean[][] visited;
-    static List<Integer> answer = new ArrayList<>();
+    static ArrayList<Integer> answers = new ArrayList<>();
 
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        sc.nextLine();
-        apt = new char[N][N];
+    public static void main(String[] args) throws Exception {
+        /**
+         * 1. 지도 초기화
+         * 2. 반복문 돌면서 방문 여부와 집 존재 여부 체크
+         * 2-1. 두 조건 만족 시 너비 우선 탐색
+         * 2-2. 너비 우선 탐색 완료 시 단지 내 아파트 수 return
+         * 2-3. 총 단지 리스트에 삽입
+         * 3. 리스트 오름차순 정렬
+         * 4. 출력
+         */
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        N = Integer.parseInt(br.readLine());
+
+        map = new int[N][N];
         visited = new boolean[N][N];
 
         for(int i = 0; i < N; i++) {
-            String homes = sc.nextLine();
+            char[] chArr = br.readLine().toCharArray();
             for(int j = 0; j < N; j++) {
-                apt[i][j] = homes.charAt(j);
+                map[i][j] = chArr[j] - '0';
             }
         }
 
         for(int i = 0; i < N; i++) {
             for(int j = 0; j < N; j++) {
-                if(!visited[i][j]) {
-                   int count = BFS(i,j);
-                   if(count > 0) {
-                       answer.add(count);
-                   }
+                if(isNotVisitApartment(i, j)) {
+                    answers.add(BFS(i, j));
                 }
             }
         }
 
+        Collections.sort(answers);
 
-        Collections.sort(answer);
-        System.out.println(answer.size());
-        for(int i : answer) {
-            System.out.println(i);
+        print(sb);
+
+        bw.write(sb.toString());
+        bw.flush();
+        br.close();
+        bw.close();
+
+    }
+
+    private static void print(StringBuilder sb) {
+        sb.append(answers.size()).append("\n");
+
+        for(int i : answers) {
+            sb.append(i).append("\n");
         }
     }
 
@@ -52,28 +70,33 @@ public class Main {
         q.add(new int[] {i, j});
         visited[i][j] = true;
 
-        if(apt[i][j] == '0') {
-            return 0;
-        }
-        count = 1;
+        int count = 1;
 
         while(!q.isEmpty()) {
             int[] now = q.poll();
 
-            for(int k =0; k < 4; k++) {
-                int x = now[0] + dx[k];
-                int y = now[1] + dy[k];
-                if(x >= 0 && y >= 0 && x < N && y < N) {
-                    if(apt[x][y] == '1' && !visited[x][y]) {
-                        count += 1;
-                        visited[x][y] = true;
-                        q.add(new int[] {x, y});
-                    }
+            for(int k = 0; k < 4; k++) {
+                int nx = now[0] + dx[k];
+                int ny = now[1] + dy[k];
+
+                if(nx < 0 || ny < 0 || nx >= N || ny >= N || visited[nx][ny]) {
+                    continue;
+                }
+
+                if(map[nx][ny] == 1) {
+                    visited[nx][ny] = true;
+                    q.add(new int[] {nx, ny});
+                    count += 1;
                 }
             }
         }
+
         return count;
     }
+
+    private static boolean isNotVisitApartment(int i, int j) {
+        return !visited[i][j] && map[i][j] == 1;
+    }
+
+
 }
-
-
