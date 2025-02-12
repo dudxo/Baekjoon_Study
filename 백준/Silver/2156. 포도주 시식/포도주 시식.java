19e264071fc1ba3dda184917b99d4f98c9ad5cbe
권영태 <56019823+dudxo@users.noreply.github.com>
2025-02-12 17:01:12 +0900

    [Silver I] Title: 포도주 시식, Time: 140 ms, Memory: 15676 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -1,33 +1,38 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    static int t, n, k;
-    static int[] grape;
-    static Integer[] dp;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        t = sc.nextInt();
-        grape = new int[t+1];
-        dp = new Integer[t+1];
-        for(int i = 1; i <= t; i++) {
-            grape[i] = sc.nextInt();
+    static int N;
+    static int[] wines;
+    static int[] dp;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        N = Integer.parseInt(br.readLine());
+        wines = new int[N+1];
+        dp = new int[N+1];
+
+        for(int i = 1; i <= N; i++) {
+            wines[i] = Integer.parseInt(br.readLine());
         }
-        dp[0] = 0;
-        System.out.print(maxWine());
+        for(int i = 1; i <= N; i++) {
+            if(i <= 2) {
+                dp[i] = dp[i-1]+wines[i];
+                continue;
+            }
+            dp[i] = Math.max(dp[i-1], Math.max((dp[i-3]+wines[i-1]+wines[i]), (dp[i-2]+wines[i])));
+        }
+
+        sb.append(dp[N]);
+
+        bw.write(sb.toString());
+        bw.flush();
+        br.close();
+        bw.close();
     }
 
-    private static int maxWine() {
-        for(int i = 1; i <= t; i++) {
-            if(i == 1) {
-                dp[i] = grape[1];
-            }
-            else if(i == 2) {
-                dp[i] = grape[i] + grape[i-1];
-            } else{
-                dp[i] = Math.max(dp[i-1], Math.max(dp[i-2]+grape[i], dp[i-3] + grape[i-1] + grape[i]));
-            }
-        }
-        return dp[t];
-    }
 }
