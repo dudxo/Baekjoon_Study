23905371a1738d4783a4843907cd66d7419b85ab
권영태 <56019823+dudxo@users.noreply.github.com>
2025-02-12 15:45:32 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 632 ms, Memory: 53780 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -4,8 +4,7 @@ import java.util.*;
 public class Main {
 
     static int N;
-    static Node[] arr;
-    public static class Node implements Comparable<Node> {
+    static class Node implements Comparable<Node> {
         int x;
         int y;
 
@@ -19,14 +18,11 @@ public class Main {
             if(this.x == o.x) {
                 return Integer.compare(this.y, o.y);
             }
+
             return Integer.compare(this.x, o.x);
         }
-
-        @Override
-        public String toString() {
-            return x + " " + y + "\n";
-        }
     }
+    static List<Node> list = new ArrayList<>();
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -35,27 +31,23 @@ public class Main {
         StringTokenizer str;
 
         N = Integer.parseInt(br.readLine());
-        arr = new Node[N];
-
         for(int i = 0; i < N; i++) {
             str = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(str.nextToken());
             int y = Integer.parseInt(str.nextToken());
-
-            arr[i] = new Node(x, y);
+            list.add(new Node(x, y));
         }
 
-        Arrays.sort(arr);
-        for(Node n : arr) {
-            sb.append(n.toString());
-        }
+        Collections.sort(list);
 
+        for(Node n : list) {
+            sb.append(n.x).append(" ").append(n.y).append("\n");
+        }
 
         bw.write(sb.toString());
         bw.flush();
         br.close();
         bw.close();
     }
-
 
 }
