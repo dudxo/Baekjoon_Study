5dac5e933c20ada12c570b9903d3039f99718099
권영태 <56019823+dudxo@users.noreply.github.com>
2025-02-13 13:32:28 +0900

    [Platinum V] Title: 찾기, Time: 468 ms, Memory: 40904 KB -BaekjoonHub

---
+++ 백준/Platinum/1786. 찾기/찾기.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int st, ed, count, totalLen;
+    static String text;
+    static String pattern;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        text = br.readLine();
+        pattern = br.readLine();
+
+        kmp(text, pattern, sb);
+
+        bw.write(count+"\n");
+        bw.write(sb.toString());
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+
+    private static void kmp(String text, String pattern, StringBuilder sb) {
+        char[] texts = text.toCharArray();
+        char[] patterns = pattern.toCharArray();
+        int[] makeTable = table(pattern);
+
+        int k = 0;
+
+        for(int i = 0; i < texts.length; i++) {
+           while(k > 0 && texts[i] != patterns[k]) {
+               k = makeTable[k-1];
+           }
+           if(texts[i] == patterns[k]) {
+               if(k == patterns.length-1) {
+                   count++;
+                   k = makeTable[k];
+                   sb.append(i-patterns.length+2).append(' ');
+               } else {
+                   k++;
+               }
+           }
+        }
+    }
+
+    private static int[] table(String pattern) {
+        char[] patterns = pattern.toCharArray();
+        int[] table = new int[patterns.length];
+        int k = 0;
+
+        for(int i = 1; i < patterns.length; i++) {
+            while(k > 0 && patterns[i] != patterns[k]) {
+                k = table[k-1];
+            }
+            if(patterns[i] == patterns[k]) {
+                table[i] = ++k;
+            }
+        }
+        return table;
+    }
+
+}
