d6b43c79fbd832421e5a1e992980e8ca8550d824
권영태 <56019823+dudxo@users.noreply.github.com>
2025-02-28 09:33:22 +0900

    [Silver I] Title: 회전 초밥, Time: 1028 ms, Memory: 23160 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.java
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.java
@@ -22,15 +22,15 @@ public class Main {
         k = Integer.parseInt(str.nextToken());
         c = Integer.parseInt(str.nextToken());
 
-        arr = new int[N*2];
+        arr = new int[N * 2];
         eat = new int[k];
 
-        for(int i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-        for(int i = N; i < N*2; i++) {
-            arr[i] = arr[i%N];
+        for (int i = N; i < N * 2; i++) {
+            arr[i] = arr[i % N];
         }
 
         sol();
@@ -49,22 +49,22 @@ public class Main {
         int count = 0;
 
         q.add(arr[st]);
-        if(arr[st] != c) {
+        if (arr[st] != c) {
             count += 1;
         }
 
-        while(st < N*2 && ed < N*2) {
-            if(ed - st == k) {
-                result = Math.max(result, count+1);
+        while (st < N * 2 && ed < N * 2) {
+            if (ed - st == k) {
+                result = Math.max(result, count + 1);
                 int tmp = q.poll();
-                if(!q.contains(tmp) && tmp != c) {
+                if (!q.contains(tmp) && tmp != c) {
                     count -= 1;
                 }
             }
 
-            if(ed - st < k) {
-                if(arr[ed] != c && !q.contains(arr[ed])) {
-                    count +=1;
+            if (ed - st < k) {
+                if (arr[ed] != c && !q.contains(arr[ed])) {
+                    count += 1;
                 }
                 q.add(arr[ed]);
                 ed++;
@@ -72,21 +72,5 @@ public class Main {
                 st++;
             }
         }
-
-        if(ed - st == k) {
-            result = Math.max(result, count);
-            count = setCount();
-            q.poll();
-        }
     }
-
-    private static int setCount() {
-        Set<Integer> set = new HashSet<>();
-        for(int i : q) {
-            set.add(i);
-        }
-
-        return set.size();
-    }
-
 }
