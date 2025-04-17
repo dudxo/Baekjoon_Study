017728d707d10abdb2fd5c1c012f3b8670ae896f
권영태 <56019823+dudxo@users.noreply.github.com>
2025-04-17 09:47:27 +0900

    [Gold V] Title: 퇴사 2, Time: 1012 ms, Memory: 397908 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	static int N;
+	static int[] dp;
+	static int[][] arr;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer str;
+
+		N = Integer.parseInt(br.readLine());
+
+		arr = new int[N][2];
+		dp = new int[N];
+		Arrays.fill(dp, -1);
+		for(int i = 0; i < N; i++) {
+			str = new StringTokenizer(br.readLine());
+			arr[i][0] = Integer.parseInt(str.nextToken());
+			arr[i][1] = Integer.parseInt(str.nextToken());
+		}
+
+
+		sb.append(sol(0));
+
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
+
+	private static int sol(int day) {
+		if(day > N) {
+			return Integer.MIN_VALUE;
+		}
+
+		if(day == N) {
+			return 0;
+		}
+
+		if(dp[day] != -1) {
+			return dp[day];
+		}
+
+		return dp[day] = Integer.max(sol(day+arr[day][0]) + arr[day][1], sol(day+1));
+	}
+
+}
