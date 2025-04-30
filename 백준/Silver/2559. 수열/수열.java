548031616534c24198072cef412b35823aab4f7c
권영태 <56019823+dudxo@users.noreply.github.com>
2025-04-30 09:33:13 +0900

    [Silver III] Title: 수열, Time: 248 ms, Memory: 23936 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -1,49 +1,41 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
 
-    static int N, K, sum, result;
-    static int[] arr;
+	static int N, M;
+	static int[] sum;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer str;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer str;
 
-        str = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(str.nextToken());
-        K = Integer.parseInt(str.nextToken());
+		str = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(str.nextToken());
+		M = Integer.parseInt(str.nextToken());
+		sum = new int[N+1];
 
-        arr = new int[N];
-        str = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(str.nextToken());
-        }
+		str = new StringTokenizer(br.readLine());
+		for(int i = 1; i <= N; i++) {
+			sum[i] = Integer.parseInt(str.nextToken()) + sum[i - 1];
+		}
 
-        for(int i = 0; i < K; i++) {
-            sum += arr[i];
-        }
+		int i = 1, max = Integer.MIN_VALUE;
+		do {
+			if(i-M < 0) {
+				continue;
+			}
 
-        sol();
+			max = Math.max(max, sum[i] - sum[i - M]);
+		} while(++i <= N);
 
-        bw.write(String.valueOf(result));
-        bw.flush();
-    }
+		sb.append(max);
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
 
-    private static void sol() {
-        int st = 0;
-        int ed = K;
-
-        result = sum;
-
-        while(ed != N) {
-            sum -= arr[st++];
-            sum += arr[ed++];
-
-            result = Math.max(result, sum);
-        }
-    }
 
 }
