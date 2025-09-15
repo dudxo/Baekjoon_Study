953b52a729609cd90e5931dc36d5679eb5079c87
권영태 <56019823+dudxo@users.noreply.github.com>
2025-09-15 18:08:15 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 73.68 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] gems) {
+        int[] answer = {1, gems.length};
+        
+        Set<String> set = new HashSet<>();
+        for(String s : gems) {
+            set.add(s);
+        }
+        int size = set.size();
+        
+        Map<String, Integer> map = new HashMap<>();
+        map.put(gems[0], 1);
+        
+              
+        int s = 0, e = 0;
+        
+        while(s <= e) {
+            // 모든 보석 갖고 있고, 구간이 더 짧음
+            if(map.size() == size && (e - s) < (answer[1] - answer[0]) ) {
+                answer[0] = s + 1;
+                answer[1] = e + 1;
+            }
+            
+            // 모든 보석보다 적음
+            if(map.size() < size) {
+                e++;
+                
+                if(e == gems.length) break;
+                map.merge(gems[e], 1, Integer::sum);
+            }
+                        
+            else {
+                map.merge(gems[s], -1, Integer::sum);
+                if(map.get(gems[s]) == 0) map.remove(gems[s]);
+                s++;
+            }
+        }
+        
+        
+        
+        return answer;
+    }
+}
