ff0c79f349c75d0ae88982f4ca08c3ce3a4437ab
권영태 <56019823+dudxo@users.noreply.github.com>
2026-03-26 21:54:14 +0900

    [Silver I] Title: 단지번호붙이기, Time: 124 ms, Memory: 15932 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,102 +1,69 @@
-
 import java.io.*;
 import java.util.*;
 
+
 public class Main {
 
-    static int N;
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {1, 0, -1, 0};
-    static int[][] map;
-    static boolean[][] visited;
-    static ArrayList<Integer> answers = new ArrayList<>();
+	static char[][] map;
+	static boolean[][] visited;
+	static int N, M, cnt;
+	static int[] dx = {0, 1, 0, -1};
+	static int[] dy = {1, 0, -1, 0};
 
-    public static void main(String[] args) throws Exception {
-        /**
-         * 1. 지도 초기화
-         * 2. 반복문 돌면서 방문 여부와 집 존재 여부 체크
-         * 2-1. 두 조건 만족 시 너비 우선 탐색
-         * 2-2. 너비 우선 탐색 완료 시 단지 내 아파트 수 return
-         * 2-3. 총 단지 리스트에 삽입
-         * 3. 리스트 오름차순 정렬
-         * 4. 출력
-         */
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        N = Integer.parseInt(br.readLine());
+		N = Integer.parseInt(br.readLine());
 
-        map = new int[N][N];
-        visited = new boolean[N][N];
+		map = new char[N][N];
+		visited = new boolean[N][N];
 
-        for(int i = 0; i < N; i++) {
-            char[] chArr = br.readLine().toCharArray();
-            for(int j = 0; j < N; j++) {
-                map[i][j] = chArr[j] - '0';
-            }
-        }
+		cnt = 0;
 
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < N; j++) {
-                if(isNotVisitApartment(i, j)) {
-                    answers.add(BFS(i, j));
-                }
-            }
-        }
+		int p = 0, q = 0;
+		String s = "";
 
-        Collections.sort(answers);
+		for(int i = 0; i < N; i++) {
+			s = br.readLine();
+			map[i] = s.toCharArray();
+		}
 
-        print(sb);
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
 
-        bw.write(sb.toString());
-        bw.flush();
-        br.close();
-        bw.close();
+		for(int i = 0; i < N; i++) {
+			for(int j = 0 ; j < N; j++) {
+				if(map[i][j] == '1' && !visited[i][j]) {
+					cnt = 0;
+					dfs(i, j);
+					pq.add(cnt);
+				}
+			}
+		}
 
-    }
+		bw.write(pq.size() + "\n");
+		while(!pq.isEmpty()) {
+			bw.write(String.valueOf(pq.poll()) + "\n");
+		}
+		bw.flush();
+		bw.close();
+	}
 
-    private static void print(StringBuilder sb) {
-        sb.append(answers.size()).append("\n");
+	private static void dfs(int x, int y) {
+		visited[x][y] = true;
+		cnt++;
 
-        for(int i : answers) {
-            sb.append(i).append("\n");
-        }
-    }
+		for(int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
 
-    private static int BFS(int i, int j) {
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[] {i, j});
-        visited[i][j] = true;
+			if(nx < 0 || ny < 0 || nx >= N || ny >= N || map[nx][ny] == '0' || visited[nx][ny]) {
+				continue;
+			}
 
-        int count = 1;
-
-        while(!q.isEmpty()) {
-            int[] now = q.poll();
-
-            for(int k = 0; k < 4; k++) {
-                int nx = now[0] + dx[k];
-                int ny = now[1] + dy[k];
-
-                if(nx < 0 || ny < 0 || nx >= N || ny >= N || visited[nx][ny]) {
-                    continue;
-                }
-
-                if(map[nx][ny] == 1) {
-                    visited[nx][ny] = true;
-                    q.add(new int[] {nx, ny});
-                    count += 1;
-                }
-            }
-        }
-
-        return count;
-    }
-
-    private static boolean isNotVisitApartment(int i, int j) {
-        return !visited[i][j] && map[i][j] == 1;
-    }
-
+			dfs(nx, ny);
+		}
+	}
 
 }
