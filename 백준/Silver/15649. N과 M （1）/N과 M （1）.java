dde7042cb984595bffd5dca0416859163d1f860d
권영태 <56019823+dudxo@users.noreply.github.com>
2026-03-28 16:17:14 +0900

    [Silver III] Title: N과 M (1), Time: 288 ms, Memory: 35212 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,49 +1,51 @@
 import java.io.*;
 import java.util.*;
 
+
 public class Main {
 
-    static int N, M;
-    static int[] arr;
-    static boolean[] isUsed;
+	static int N, M;
+	static int[] arr;
+	static boolean[] isUsed;
+	static StringBuilder sb;
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        StringTokenizer str = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(str.nextToken());
-        M = Integer.parseInt(str.nextToken());
+		sb = new StringBuilder();
 
-        arr = new int[M+1];
-        isUsed = new boolean[N+1];
+		StringTokenizer str = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(str.nextToken());
+		M = Integer.parseInt(str.nextToken());
 
-        backTracking(0, sb);
+		arr = new int[M];
+		isUsed = new boolean[N+1];
 
-        bw.write(sb.toString());
-        bw.flush();
-        br.close();
-        bw.close();
-    }
+		sol(1);
 
-    private static void backTracking(int k, StringBuilder sb) {
-        if(k == M) {
-            for(int i = 1; i <= M; i++) {
-                sb.append(arr[i]).append(" ");
-            }
-            sb.append("\n");
-            return;
-        }
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
 
-        for(int i = 1; i <= N; i++) {
-            if(!isUsed[i]) {
-                arr[k+1] = i;
-                isUsed[i] = true;
-                backTracking(k+1, sb);
-                isUsed[i] = false;
-            }
-        }
-    }
+	private static void sol(int depth) {
+		if(depth > M) {
+			for(int i : arr) {
+				sb.append(i + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		for(int i = 1; i <= N; i++) {
+			if(!isUsed[i]) {
+				arr[depth-1] = i;
+				isUsed[i] = true;
+				sol(depth+1);
+				isUsed[i] = false;
+			}
+		}
+	}
 
 }
